4aee8f2d55b971f0f44da43cee2e2553927188ba
Dan Kreiger <dan@dankreiger.com>
2018-01-01 03:49:20 +0100

    More updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const r = new Router(
   {
-    german: new Layout(new Page('menu.html'), new Page('german.html')),
-    russian: new Layout(new Page('menu.html'), new Page('russian.html')),
+    german_submenu: new Layout(new Page('german/german_submenu.html')),
+    german_lesson_1: new Layout(new Page('german/lessons/1.html')),
+    german_lesson_2: new Layout(new Page('german/lessons/2.html')),
+    russian_submenu: new Layout(new Page('russian/russian_submenu.html')),
+    russian_lesson_1: new Layout(new Page('russian/lessons/1.html')),
+    russian_lesson_2: new Layout(new Page('russian/lessons/2.html')),
     '#default': new Page('menu.html'),
   },
   document.querySelector('.main')
